edd18cd02ed52f236c2c55896f6ada23c918515d
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2025-02-23 21:50:10 +0530

    implement analytics routes

---
+++ backend/routes/analytics.routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { adminRoute, protectRoute } from "../middlewares/auth.middleware.js";
+import { getAnalyticsData } from "../controllers/analytics.controller.js";
+
+const analyticsRouter = Router();
+
+analyticsRouter.get("/", protectRoute, adminRoute, async (req, res) => {
+  try {
+    const analyticsData = await getAnalyticsData();
+
+    const endDate = new Date();
+    const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+
+    const dailySalesData = await getDailySalesData(startDate, endDate);
+    res.json({ analyticsData, dailySalesData });
+  } catch (error) {
+    console.log("Error in analytics route", error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default analyticsRouter;
