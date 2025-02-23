a39285e1ab9199fbe99ae8c40f8b3f0601efd4d2
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2025-02-23 20:48:55 +0530

    implement payment routes

---
+++ backend/routes/payment.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { protectedRoute } from "../middlewares/auth.middlewares.js";
+import {
+  checkoutSuccess,
+  createCheckoutSession,
+} from "../controllers/payment.controller.js";
+
+const paymentRouter = Router();
+
+paymentRouter.post(
+  "/create-checkout-session",
+  protectedRoute,
+  createCheckoutSession
+);
+
+paymentRouter.post("/checkout-success", protectedRoute, checkoutSuccess);
+
+export default paymentRouter;
